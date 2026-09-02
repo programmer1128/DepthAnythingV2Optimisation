e2cc4b58023fb18f90879d5a299dfe8e8c3ecc53
aritra <aritra.banerjee.school@gmail.com>
2026-09-02 18:02:48 +0530

    initial commit

---
+++ build/CMakeFiles/depth_pipeline.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aritra/Desktop/OptimisingDepthAnythingV2/Depth-Anything-V2/DepthAnythingCPP/src/DepthModel.cpp" "CMakeFiles/depth_pipeline.dir/src/DepthModel.cpp.o" "gcc" "CMakeFiles/depth_pipeline.dir/src/DepthModel.cpp.o.d"
+  "/home/aritra/Desktop/OptimisingDepthAnythingV2/Depth-Anything-V2/DepthAnythingCPP/src/ImageProcessor.cpp" "CMakeFiles/depth_pipeline.dir/src/ImageProcessor.cpp.o" "gcc" "CMakeFiles/depth_pipeline.dir/src/ImageProcessor.cpp.o.d"
+  "/home/aritra/Desktop/OptimisingDepthAnythingV2/Depth-Anything-V2/DepthAnythingCPP/src/main.cpp" "CMakeFiles/depth_pipeline.dir/src/main.cpp.o" "gcc" "CMakeFiles/depth_pipeline.dir/src/main.cpp.o.d"
+  "" "depth_pipeline" "gcc" "CMakeFiles/depth_pipeline.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/depth_pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/depth_pipeline.dir/link.d"
+  "CMakeFiles/depth_pipeline.dir/src/DepthModel.cpp.o"
+  "CMakeFiles/depth_pipeline.dir/src/DepthModel.cpp.o.d"
+  "CMakeFiles/depth_pipeline.dir/src/ImageProcessor.cpp.o"
+  "CMakeFiles/depth_pipeline.dir/src/ImageProcessor.cpp.o.d"
+  "CMakeFiles/depth_pipeline.dir/src/main.cpp.o"
+  "CMakeFiles/depth_pipeline.dir/src/main.cpp.o.d"
+  "depth_pipeline"
+  "depth_pipeline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/depth_pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
